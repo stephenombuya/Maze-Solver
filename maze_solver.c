113ce70c20be58f6c42389e6282bda5cf178f5ad
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-03 09:41:34 +0300

    Update maze_solver.c

--- maze_solver.c
+++ maze_solver.c
@@ -1,54 +1,65 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define N 5 // Dimension of the maze
 
 // Function prototypes
-int solveMaze(int maze[N][N], int x, int y, int solution[N][N]);
+bool solveMaze(int maze[N][N], int x, int y, int solution[N][N]);
 void printSolution(int solution[N][N]);
+void initializeSolution(int solution[N][N]);
 
 // Check if (x, y) is a valid move
-int isSafe(int maze[N][N], int x, int y) {
-    return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 0);
+bool isSafe(int maze[N][N], int x, int y, int solution[N][N]) {
+    return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 0 && solution[x][y] == 0);
+}
+
+// Function to initialize the solution matrix
+void initializeSolution(int solution[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            solution[i][j] = 0;
+        }
+    }
 }
 
 // Main function to solve the maze
-int solveMaze(int maze[N][N], int x, int y, int solution[N][N]) {
+bool solveMaze(int maze[N][N], int x, int y, int solution[N][N]) {
     // If (x, y) is the bottom-right corner, the maze is solved
-    if (x == N - 1 && y == N - 1) {
+    if (x == N - 1 && y == N - 1 && maze[x][y] == 0) {
         solution[x][y] = 1;
-        return 1;
+        return true;
     }
 
     // Check if maze[x][y] is a valid move
-    if (isSafe(maze, x, y)) {
+    if (isSafe(maze, x, y, solution)) {
         // Mark x, y as part of the solution path
         solution[x][y] = 1;
 
         // Move right
         if (solveMaze(maze, x, y + 1, solution)) {
-            return 1;
+            return true;
         }
 
         // Move down
         if (solveMaze(maze, x + 1, y, solution)) {
-            return 1;
+            return true;
         }
 
         // Move left
         if (solveMaze(maze, x, y - 1, solution)) {
-            return 1;
+            return true;
         }
 
         // Move up
         if (solveMaze(maze, x - 1, y, solution)) {
-            return 1;
+            return true;
         }
 
         // Backtrack: unmark x, y as part of the solution path
         solution[x][y] = 0;
     }
 
-    return 0;
+    return false;
 }
 
 // Function to print the solution
@@ -71,7 +82,8 @@ int main() {
         {0, 0, 0, 1, 0}
     };
 
-    int solution[N][N] = {0};
+    int solution[N][N];
+    initializeSolution(solution);
 
     if (solveMaze(maze, 0, 0, solution)) {
         printf("Solution:\n");
